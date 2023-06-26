d2c1bc93ba961966b15fa94dfe703dc829c782b8
Harsh Raj <81627508+hraj26058@users.noreply.github.com>
2023-06-26 13:43:26 +0530

    Create Collider.js

---
+++ components/Collider.js
@@ -0,0 +1,35 @@
+AFRAME.registerComponent("flying-birds",{
+init:function(){
+    for (var i = 1; i <= 20; i++) {
+        //id
+        var id = `birds${i}`;
+  
+        //position variables     
+        var posX =(Math.random() * 3000 + (-1000));      
+        var posY = (Math.random() * 2 + (-1));
+        var posZ = (Math.random() * 3000 + -1000);
+  
+        var position = { x: posX, y: posY, z: posZ };
+  
+        //call the function
+        this.flyingBirds(id, position);
+      }
+    } ,
+
+    flyingBirds: function(id, position) { 
+    
+        var terrainEl = document.querySelector("#terrain");
+    
+        var birdsEl = document.createElement("a-entity");
+    
+        birdsEl.setAttribute("id",id);
+        birdsEl.setAttribute("position",position);
+        
+        birdsEl.setAttribute("scale",{x:500,y:500,z:500});
+        
+        birdsEl.setAttribute("gltf-model","./assets/models/flying_bird/scene.gltf");   
+        birdsEl.setAttribute("animation-mixer",{})
+    
+        terrainEl.appendChild(birdsEl);
+      }
+})
